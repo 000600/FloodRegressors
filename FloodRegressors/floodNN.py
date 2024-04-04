0a6e96352596e293363bb21842f8f6e360ef23d1
Aiden Gandhi <90153180+000600@users.noreply.github.com>
2024-04-03 22:59:39 -0600

    Update floodNN.py

--- FloodRegressors/floodNN.py
+++ FloodRegressors/floodNN.py
@@ -1,9 +1,9 @@
 # Imports
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, BatchNormalization, Dropout
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.optimizers import Adam
+from keras.layers import Dense, BatchNormalization, Dropout
+from keras.models import Sequential
+from keras.callbacks import EarlyStopping
+from keras.optimizers import Adam
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -13,22 +13,15 @@ from sklearn.metrics import mean_squared_error
 df = pd.read_csv('flood.csv')
 df = pd.DataFrame(df)
 
-x = []
-y = []
-
-# Add specific parts of the dataset to x and y lists
-for row in range(df.shape[0]):
-  rows = []
-  for point in range(len(df.loc[0]) - 1): # "- 1" because we don't want to add the last column (the labels) to the inputs section
-    rows.append(df.iloc[row][point])
-  x.append(rows)
-  y.append(df.loc[row][-1])
+# Initialize x and y sets
+y = df['FloodProbability']
+x = df.drop(labels = ['FloodProbability'], axis = 1)
 
 # Divide the x and y values into three sets: train, test, and validation
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 1)
 
 # Get input shape
-input_shape = len(x[0])
+input_shape = x.shape[1]
 
 # Create Adam optimizer
 opt = Adam(learning_rate = 0.001)
@@ -54,8 +47,11 @@ model.compile(optimizer = opt, loss = 'mse', metrics = ['mse'])
 early_stopping = EarlyStopping(min_delta = 0.001, patience = 10, restore_best_weights = True)
 
 # Train model and store training history
-epochs = 2
+epochs = 10
 history = model.fit(x_train, y_train, epochs = epochs, validation_data = (x_test, y_test), callbacks = [early_stopping])
+
+file = 'FloodNN%03depochs.h5' % (epochs)
+model.save(file)
 
 # Visualize training and validation mean squared error
 history_dict = history.history
@@ -77,8 +73,6 @@ print("\nTest Mean Squared Error (MSE):", mse)
 
 # Compare model's prediction on an input to the actual label corresponding to that input (change the index to view a different input and output set)
 index = 0
-prediction = predictions[index][0]
-label = y_test[index]
 
-print(f"\nModel's Predicted Flood Chance on Sample Input: {prediction}")
-print(f"Actual Flood Chance of Sample Input: {label}")
+print(f"Model's Prediction on a Sample Input: {predictions[index]}")
+print(f"Actual Label on the Same Input: {y_test.iat[index]}\n")
